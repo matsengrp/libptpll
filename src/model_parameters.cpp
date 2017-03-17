70642f4dca4125c604329291d9fe0a4da95e272c
Brian Claywell <bclaywel@fhcrc.org>
2017-03-17 16:28:19 -0700

    Initial commit.

---
+++ src/model_parameters.cpp
@@ -0,0 +1,72 @@
+#include "model_parameters.hpp"
+
+#include <fstream>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+#include "pll_util.hpp"
+
+namespace pt { namespace pll {
+
+/// @brief Partition string according to delimiter.
+std::vector<std::string> ssplit(const std::string &s, char delim) {
+    std::stringstream ss(s);
+    std::string item;
+    std::vector<std::string> tokens;
+    while (getline(ss, item, delim)) {
+        tokens.push_back(item);
+    }
+    return tokens;
+}
+
+ModelParameters ParseRaxmlInfo(const std::string& path)
+{
+  std::ifstream file(path);
+  std::string read;
+  std::string contents;
+
+  while (std::getline(file, read)) {
+    contents += read;
+    contents.push_back('\n');
+  }
+
+  // initialize the array of base frequencies
+  std::size_t pos1 = contents.find("frequencies: ");
+  std::size_t pos2 = contents.find('\n', pos1);
+  std::string sstr = contents.substr(pos1 + 13, pos2 - pos1 - 13);
+
+  std::vector<std::string> freqvector = ssplit(sstr, ' ');
+  std::vector<double> frequencies(freqvector.size());
+
+  for (unsigned int i = 0; i < freqvector.size(); i++)
+    frequencies[i] = std::stod(freqvector.at(i));
+
+  // initialize the array of subst_params
+  pos1 = contents.find("ac ag at cg ct gt:");
+  pos2 = contents.find('\n', pos1);
+  sstr = contents.substr(pos1 + 19, pos2 - pos1 - 19);
+  std::vector<std::string> ratevector = ssplit(sstr, ' ');
+  std::vector<double> subst_params(ratevector.size());
+
+  for (unsigned int i = 0; i < ratevector.size(); i++)
+    subst_params[i] = std::stod(ratevector.at(i));
+  if (ratevector.size() != (((freqvector.size()) * freqvector.size() - 4) / 2)) {
+    throw std::invalid_argument("Wrong number of rate values.");
+  }
+
+  // we'll use RATE_CATS rate categories, and currently initialize them to 0
+  std::vector<double> rate_cats(RATE_CATS, 0.0);
+
+  // initialize the alpha value
+  pos1 = contents.find("alpha[0]: ");
+  pos2 = contents.find(' ', pos1 + 10);
+  sstr = contents.substr(pos1 + 10, pos2 - pos1 - 10);
+  double alpha = stod(sstr);
+
+  ModelParameters parameters{alpha, frequencies, subst_params};
+  return parameters;
+}
+
+} } // namespace pt::pll
---
+++ src/pll_util.hpp
@@ -0,0 +1,45 @@
+#ifndef PT_PLL_UTIL_HPP_
+#define PT_PLL_UTIL_HPP_
+
+#include <string>
+#include <vector>
+
+// pll.h is missing a header guard
+#ifndef LIBPLL_PLL_H_
+#define LIBPLL_PLL_H_
+#include <libpll/pll.h>
+#endif
+
+/// @file pll_util.hpp
+/// @brief Utilities for interfacing with libpll.
+/// Much of this was directly copied from libpll examples, so parts (c) Thomas
+/// Flouri.
+
+namespace pt { namespace pll {
+
+const unsigned int STATES = 4;
+const unsigned int RATE_CATS = 4;
+const unsigned int ALIGNMENT = PLL_ALIGNMENT_AVX;
+const unsigned int ARCH_FLAGS = PLL_ATTRIB_ARCH_AVX;
+
+typedef struct { int clv_valid; } node_info_t;
+
+enum class TraversalType { FULL, PARTIAL };
+
+int cb_full_traversal(pll_utree_t *node);
+int cb_partial_traversal(pll_utree_t *node);
+int cb_copy_clv_traversal(pll_utree_t *node);
+int cb_branch_healthy(pll_utree_t *tree);
+int cb_erase_data(pll_utree_t *tree);
+
+pll_partition_t *pllext_partition_clone(pll_partition_t *rhs);
+
+bool TreeHealthy(pll_utree_t *tree);
+
+unsigned int ParseFasta(std::string path, unsigned int seq_count,
+                        std::vector<std::string> &headers_out,
+                        std::vector<std::string> &seqdata_out);
+
+} } // namespace pt::pll
+
+#endif // PT_PLL_UTIL_HPP_
